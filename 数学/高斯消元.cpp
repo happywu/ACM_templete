6996a2eb561ce174ea0c3e015c2f88a12dca5b81
happywu <happywunaiping@gmail.com>
2015-08-06 10:38:38 +0800

    Xor updated
    XorGuass updated

--- 数学/高斯消元.cpp
+++ 数学/高斯消元.cpp
@@ -2,31 +2,36 @@
 #include<iostream>
 using namespace std;
 const int maxn = 100;
-double Matrix[maxn][maxn];
+typedef double Matrix[maxn][maxn];
 
-//要求系数矩阵可逆
-//A为增广矩阵，A[i][n]为第i个方程右边的常数bi
-void gauss_elimination(Matrix A,int n){
+int gauss_elimination(Matrix &A){
 	for(int i=0;i<n;i++){
-		//选一行r与第i行交换，取绝对值最大的一行
 		int r=i;
-		for(int j=i+1;j<n;j++)
-			if(fabs[A[j][i]]>fabs[A[r][i]]) r=j;
-		if(r!=i)for(int j=0;j<=n;j++) swap(A[r][j],A[i][j]);
-
+		//选一行r与第i行交换，取绝对值最大的一行
+		for(int j=i+1;j<m;j++)
+			if(fabs(A[j][i])>fabs(A[r][i]))r=j;
+		if(r!=i)for(int j=0;j<=n;j++)swap(A[r][j],A[i][j]);
+		if(A[i][i]==0)return 2;//多解
 		//消元
-		for(int k=i+1;k<n;k++){
+		for(int k=i+1;k<m;k++){
 			double f=A[k][i]/A[i][i];
 			for(int j=i;j<=n;j++)A[k][j]-=f*A[i][j];
 		}
 	}
-
+	for(int i=0;i<m;i++){
+		bool flag=0;
+		for(int j=0;j<n;j++){
+			if(A[i][j]!=0){flag=1;break;}
+		}
+		if(!flag&&A[i][n]!=0)return 0;//无解
+	}
 	//回代
 	for(int i=n-1;i>=0;i--){
 		for(int j=i+1;j<n;j++)
 			A[i][n]-=A[j][n]*A[i][j];
 		A[i][n]/=A[i][i];
 	}
+	return 1;//有唯一解
 }
 
 int Gauss(int equ,int var){
@@ -61,3 +66,33 @@ int Gauss(int equ,int var){
 		A[i][n]/=A[i][i];
 	}
 }
+
+int XorGuass(int equ, int var) {
+    int row = 0, col = 0;
+    int free_var = 0; //自由变元数量
+    while(row < equ && col < var) {
+        int i;
+        for(i = row; i < equ; i++) if(A[i][col] != 0) break;
+        if(i == equ) { //当前主元均为0，自由变元+1。
+            free_var++;
+            col++;
+            continue;
+        }
+        for(int j = col; j <= var; j++) swap(A[i][j], A[row][j]); //将主元不为0的行交换到上边
+        for(int j = row + 1; j < equ; j++) { //异或消元
+            if(A[j][col]) {
+                for(int k = col; k <= var; k++) A[j][k] ^= A[row][k];
+            }
+        }
+        row++; col++;
+    }
+    for(int i = row; i < equ; i++) if(A[i][var]) return -1;  //无解
+    for(int i = var - 1; i >= 0; i--) {
+        ans[i] = A[i][var];
+        for(int j = i + 1; j < var; j++) {
+            ans[i] ^= (A[i][j] & ans[j]);
+        }
+    }
+    return free_var;
+}
+
